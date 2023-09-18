2a62a04a0cd561c9c612b8bf3fd68b67dca1f3b0
ABanc0 <144232727+ABanc0@users.noreply.github.com>
2023-09-18 16:00:09 +0200

    Add files via upload

---
+++ ssl-checker5/ssl_checker/ssl_checker_app/forms.py
@@ -0,0 +1,5 @@
+from django import forms 
+
+class VideoSearchForm(forms.Form):
+    search_query = forms.CharField(max_length=100, required=False)
+    clear_search = forms.BooleanField(initial=False, widget=forms.HiddenInput)
---
+++ ssl-checker5/ssl_checker/ssl_checker_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('ssl_checker_app/', views.index, name='index'),
+    path('check_ssl/', views.check_ssl, name='check_ssl'),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('home/', views.home, name='home'),
+    path('ssl_data/', views.ssl_data_view, name="ssl_data" ),
+    path('films/',  views.video_list, name="films"),
+    path('videos/play/<str:video_name>/',views.play_video, name='play_video')
+]
+
